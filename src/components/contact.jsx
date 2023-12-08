546ec0c179bad1aede6b9abd75a1f137592e6d58
Coderbutgs <gauravkumar9303@gmail.com>
2023-12-09 03:31:38 +0530

    Completed the contact and resume section

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,5 +1,34 @@
 export default function Contact(){
     return(
-        <div></div>
+        <section className = "contact-section">
+            <div className = "contact-outer-container">
+                <div className = "contact-heading-flex-container">
+                    <div className = "contact-heading-main-container">
+                        <h1>Contact Me</h1>
+                    </div>
+                </div>
+                <div className = "contact-info-flex-container">
+                    <div className = "contact-info-main-container">
+                        <h3>Contact info</h3>
+                        <div className = "contact-info-details-container">
+                            <div className = "contact-info-item">
+                                <span><img src = "../../public/images/mail_icon.svg" alt = "icon" /></span>
+                                <span>gauravpro9303@gmail.com</span>
+                            </div>
+
+                            <div className = "contact-info-item">
+                                <span><img src = "../../public/images/linkedin_icon.svg" alt = "icon" /></span>
+                                <span><a href = "https://www.linkedin.com/in/gaurav-singh-2016b3243">https://www.linkedin.com/in/gaurav-singh-2016b3243</a></span>
+                            </div>
+
+                            <div className = "contact-info-item">
+                                <span><img src = "../../public/images/github_icon.svg" alt = "icon" /></span>
+                                <span><a href = "https://github.com/Coderbutgs">https://github.com/Coderbutgs</a></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,5 +1,66 @@
 export default function Resume(){
     return(
-        <div></div>
+        <section className = "resume-section">
+            <div className = "resume-outer-container">
+                <div className = "resume-heading-flex-container">
+                    <div className = "resume-heading-main-container">
+                        <h1>Resume</h1>
+                    </div>
+                </div>
+                <div className = "resume-content-flex-container">
+                    <div className = "resume-content-img-container">
+                        <img src = "../../public/images/IMG_20231206_193341.jpg" alt = "profile" />
+                    </div>
+
+                    <div className = "resume-content-bio-container">
+                        <h3>Education</h3>
+                        <div className = "resume-bio-item-flex-container">
+                            <div className = "resume-bio-item-main-container">
+                                <h4>Bachelor in Software Engineering</h4>
+                                <div className = "clg-name">Shri Shankaracharya Technical Campus</div>
+                            </div>
+                            <div className = "date">
+                                <span>2021-2025</span>
+                            </div>
+                        </div>
+
+                        <h3 style ={{marginTop:"3rem"}}>Certificates</h3>
+                        <div className = "resume-bio-item-flex-container">
+                            <div className = "resume-bio-item-main-container">
+                                <h4>Udemy JAVA SE 15 training certification.</h4>
+                                <div className = "clg-name">Udemy</div> 
+                            </div>
+                            <div className = "date">
+                                 <span>2022</span>
+                            </div>
+                        </div>
+
+                        <div className = "resume-bio-item-flex-container">
+                            <div className = "resume-bio-item-main-container">
+                                <h4>Udemy Full Stack Web Development Training Certification</h4>
+                                <div className = "clg-name">Udemy</div> 
+                            </div>
+                            <div className = "date">
+                                 <span>2023</span>
+                            </div>
+                        </div>
+
+                        <div className = "resume-bio-item-flex-container">
+                            <div className = "resume-bio-item-main-container">
+                                <h4>Internshala Core Java and Java App development training certification.</h4>
+                                <div className = "clg-name">InternShala Trainings</div> 
+                            </div>
+                            <div className = "date">
+                                 <span>2023</span>
+                            </div>
+                        </div>
+
+                        <div className = "button-container">
+                            <a href = "https://drive.google.com/file/d/1VD44uoyecFgcmz3vWrmHIJtstn3R8_M3/view?usp=sharing">Download Resume</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
